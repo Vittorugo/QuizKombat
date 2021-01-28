69e0a79d262d63fd3dc01029a3a37b9268a0227d
victtor hugo <hgloureirocosta@gmail.com>
2021-01-28 20:39:41 -0300

    Aula 2 - State , Rotas, Linter

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
-
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -17,9 +18,10 @@ const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -28,7 +30,24 @@ export default function Home() {
             Titulo 1
           </Widget.Header>
           <Widget.Content>
-            usuahsua
+            <form onSubmit={function (infoDoEvento) {
+              infoDoEvento.preventDefault();
+
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (infoDoEvento) {
+                  setName(infoDoEvento.target.value);
+                }}
+                placeholder="Digite seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -39,9 +58,9 @@ export default function Home() {
             houhouho
           </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl='https://github.com/vittorugo'/>
+      <GitHubCorner projectUrl="https://github.com/vittorugo" />
     </QuizBackground>
-  )
+  );
 }
