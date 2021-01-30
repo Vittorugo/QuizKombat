38a37235a94c021f2bd19d2160d0fad9986d93df
victtor hugo <hgloureirocosta@gmail.com>
2021-01-30 00:49:09 -0300

    Aula 3 - Página do Quiz

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -24,29 +27,31 @@ export default function Home() {
 
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
-            Titulo 1
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
+            <p>{db.description}</p>
             <form onSubmit={function (infoDoEvento) {
               infoDoEvento.preventDefault();
 
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <input
-                onChange={function (infoDoEvento) {
-                  setName(infoDoEvento.target.value);
-                }}
+              <Input
+                name="nomeDoUser"
+                onChange={ (infoDoEvento) => { setName(infoDoEvento.target.value); }}
                 placeholder="Digite seu nome"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {' '}
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
